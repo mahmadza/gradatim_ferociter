ecd8db6a5ffd564800078e5f06e525a36ec93a02
Muhammad Zabidi <mamduh.zabidi@gmail.com>
2020-09-13 19:32:41 +0800

    Create ProcessLog.sql

---
+++ FromAWSBlog/ProcessLog.sql
@@ -0,0 +1,18 @@
+
+
+
+-- Create the destination stream that stores the response count as per the source application.
+-- This will help you determine request count per source.
+-- It also helps you determine if data is coming from unknown sources.
+CREATE STREAM "DESTINATION_SQL_STREAM" (
+applicationName VARCHAR(64),contact VARCHAR(64),
+response  SMALLINT,responseCount SMALLINT);
+
+-- Aggregrate response over joined data with host application mapping stored on S3.
+-- It always uses the latest S3 file
+CREATE OR REPLACE PUMP "DESTINATION_SQL_STREAM" AS
+INSERT INTO "DESTINATION_SQL_STREAM"
+    SELECT STREAM  metadata."ApplicationName" , metadata."Contact", logstream."response", COUNT(*) as responseCount
+                  FROM "SOURCE_SQL_STREAM_001" logstream LEFT JOIN "ApplicationHostMapping" metadata
+                  ON logstream."host" = metadata."Host"
+                  GROUP BY metadata."ApplicationName", metadata."Contact", logstream."response", FLOOR((logstream.ROWTIME - TIMESTAMP '1970-01-01 00:00:00') MINUTE / 5 TO MINUTE);
