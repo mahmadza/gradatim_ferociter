f74b8c30671ad10ea2afa8142c8eb79a8b914b14
Muhammad Zabidi <mamduh.zabidi@gmail.com>
2020-11-11 10:50:48 +0800

    Update practiceCodes.sql

--- TestSQLCodes/practiceCodes.sql
+++ TestSQLCodes/practiceCodes.sql
@@ -7,3 +7,28 @@ SELECT City FROM Customers
 Union
 SELECT City FROM Suppliers
 order by City;
+
+
+
+/* Logic Statement
+CASE
+  WHEN XXX THEN XXX
+  WHEN XXX THEN XXX
+  ELSE XXX
+END;
+*/
+SELECT Name, StandardCosts, "Price Comment" =
+  CASE
+    when StandardCost > 1000 then 'Too Expensive'
+    else 'Just Right'
+  END
+from SalesLT.Product
+
+
+
+
+
+
+
+
+/*     */
