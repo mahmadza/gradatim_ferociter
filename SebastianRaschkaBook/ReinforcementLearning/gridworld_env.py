4aaf15cc3f5914df8e9ffda27bd4d07f9c9812bf
Muhammad Zabidi <mamduh.zabidi@gmail.com>
2021-07-18 10:32:01 +0800

    Update gridworld_env.py

--- SebastianRaschkaBook/ReinforcementLearning/gridworld_env.py
+++ SebastianRaschkaBook/ReinforcementLearning/gridworld_env.py
@@ -16,7 +16,18 @@ MARGIN = 10
 def get_coords(row, col, loc="center"):
     xc = (col + 1.5) * CELL_SIZE
     yc = (row + 1.5) * CELL_SIZE
+
     if loc == "center":
         return xc, yc
 
-    return x1
+    elif loc == "interior_corners":
+        half_size = CELL_SIZE // 2 - MARGIN
+        xl, xr = xc - half_size, xc + half_size
+        yt, yb = xc - half_size, xc + half_size
+        return [(xl, yt), (xr, yt), (xr, yb), (x1, yb)]
+
+    elif loc == "interior_triangle":
+        xl, yl = xc, yc + CELL_SIZE // 3
+        x2, y2 = xc + CELL_SIZE // 3, yc - CELL_SIZE // 3
+        x3, y3 = xc - CELL_SIZE // 3, yc - CELL_SIZE // 3
+        return [(xl, yl), (x2, y2), (x3, y3)]
