1020c8b82e0b9594a95ef79ab84eb906f4b1ccc0
Muhammad Zabidi <mamduh.zabidi@gmail.com>
2021-07-18 10:28:47 +0800

    Update gridworld_env.py

--- SebastianRaschkaBook/ReinforcementLearning/gridworld_env.py
+++ SebastianRaschkaBook/ReinforcementLearning/gridworld_env.py
@@ -6,3 +6,17 @@ from collections import defaultdict
 import time
 import pickle
 import os
+
+from gym.envs.classic_control import rendering
+
+CELL_SIZE = 100
+MARGIN = 10
+
+
+def get_coords(row, col, loc="center"):
+    xc = (col + 1.5) * CELL_SIZE
+    yc = (row + 1.5) * CELL_SIZE
+    if loc == "center":
+        return xc, yc
+
+    return x1
