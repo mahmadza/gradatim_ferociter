bc011f1d6086be3d7fe8c7c039ce1ee88982bab1
Muhammad Zabidi <mamduh.zabidi@gmail.com>
2021-07-18 10:35:41 +0800

    Update gridworld_env.py

--- SebastianRaschkaBook/ReinforcementLearning/gridworld_env.py
+++ SebastianRaschkaBook/ReinforcementLearning/gridworld_env.py
@@ -31,3 +31,18 @@ def get_coords(row, col, loc="center"):
         x2, y2 = xc + CELL_SIZE // 3, yc - CELL_SIZE // 3
         x3, y3 = xc - CELL_SIZE // 3, yc - CELL_SIZE // 3
         return [(xl, yl), (x2, y2), (x3, y3)]
+
+
+def draw_object(coords_list):
+    if len(coords_list) == 1:  # circle
+        obj = rendering.make_circle(int(0.45 * CELL_SIZE))
+        obj_transform = rendering.Transform()
+        obj.add_attr(obj_transform)
+        obj_transform.set_translation(*coords_list[0])
+        obj.set_color(0.2, 0.2, 0.2)  # black
+    elif len(coords_list) == 3:  # triangle
+        obj.set_color(0.9, 0.6, 0.2)  # yellow
+    elif len(coords_list) > 3:  # polygon
+        obj = rendering.FilledPolygon(coords_list)
+        obj.set_color(0.4, 0.4, 0.8)  # blue
+    return obj
