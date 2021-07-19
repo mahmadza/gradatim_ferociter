b907e8a7b27ac5bc3e2a68acfd6285e35ee1df56
Muhammad Zabidi <mamduh.zabidi@gmail.com>
2021-07-20 00:12:06 +0800

    Update qdeeplearning.py

---
+++ SebastianRaschkaBook/ReinforcementLearning/qdeeplearning.py
@@ -0,0 +1,9 @@
+import gym
+import numpy as np
+import tensorflow as tf
+import random
+import matplotlib.pyplot as plt
+from collections import namedtuple, deque
+
+np.random.seed(1)
+tf.random.set_seed(1)
