d6d2dc63410da5c2ff1b688613d4e5984195bb91
Muhammad Zabidi <mamduh.zabidi@gmail.com>
2021-07-23 17:47:54 +0800

    Update qdeeplearning.py

--- SebastianRaschkaBook/ReinforcementLearning/qdeeplearning.py
+++ SebastianRaschkaBook/ReinforcementLearning/qdeeplearning.py
@@ -84,3 +84,13 @@ class DQNAgent:
         samples = random.sample(self.memory, batch_size)
         history = self._learn(samples)
         return history.history["loss"][0]
+
+    def plot_learning_history(history):
+        fig = plt.figure(1, figsize=(14, 5))
+        ax = fig.add_subplot(1, 1, 1)
+        episodes = np.arange(len(history[0])) + 1
+        plt.plot(episodes, history[0], lw=4, marker="o", markersize=10)
+        x.tick_params(axis="both", which="major", labelsize=15)
+        plt.xlabel("Episodes", size=20)
+        plt.ylabel("# total rewards ", size=20)
+        plt.show()
